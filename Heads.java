ee66950df08d19343083e1d746bd483004f4fbe8
s1310131 <s1310131@u-aizu.ac.jp>
2024-07-04 16:57:41 +0900

    Heads.java

--- Heads.java
+++ Heads.java
@@ -27,7 +27,7 @@ public class Heads{
     }
 
     System.out.println("Heads: "+h+", Tails: "+t);
-    if(h>t) System.out.println(name+" won");
-    else System.out.println(name" lost");
+    if(h>t) System.out.println(name+" won!");
+    else System.out.println(name" lost...");
     }
 }
